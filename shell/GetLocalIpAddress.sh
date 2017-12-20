3753b37108d0b1b3d48aa6e6bd6c681886774319
lixiaodaoaaa <lixiaodaoaaa@126.com>
2017-12-20 14:12:57 +0800

    rechange shell

--- shell/GetLocalIpAddress.sh
+++ shell/GetLocalIpAddress.sh
@@ -3,7 +3,7 @@ webHeader="http://"
 ipLine=$(ifconfig en0   | grep   "inet" | grep "broadcast" | grep "10")
 deleteRightIpLine=${ipLine% n*}
 ipResult=${deleteRightIpLine#* }
-echo ${ipResult}
 fullWebAddress=${webHeader}${ipResult}/apk/
+echo ${fullWebAddress}  | pbcopy
 echo ${fullWebAddress}
 open ${fullWebAddress}
